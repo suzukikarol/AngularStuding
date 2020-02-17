b798fb26e5c629960d7d54efc003805b0fad4957
Karoline Suzuki <ka.suzuki.23@gmail.com>
2020-02-17 16:43:10 -0300

    adição de conteúdo das aulas

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { CursosModule} from './meu-primeiro2/';
+ 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiro2Component,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ primeiro-projeto/src/app/cursos/cursos.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CursosComponent } from './cursos';
+
+
+
+@NgModule({
+  declarations: [CursosComponent],
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    CursosComponent
+  ],
+  export: [
+    CursosComponent
+  ]
+})
+export class CursosModule { }
